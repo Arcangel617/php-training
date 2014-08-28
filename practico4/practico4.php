7b98244491f7bfba46cbd1320d488250f0f5e4c3
Arcangel617 <artigue_arcangel@hotmail.com>
2014-08-28 18:08:35 -0300

    Respondí hasta el ejercicio 5

--- practico4/practico4.php
+++ practico4/practico4.php
@@ -3,44 +3,87 @@
     <meta charset="UTF-8">
 </head>
 <body>
-    1 - ¿Qué tags me conviene usar para encerrar código php?
+    <h2>Práctico 4</h2>
+    <ol>
+        <li value="1">
+            <p>¿Qué tags me conviene usar para encerrar código php?</p>
+            <p>Para poder encerrar codigo php es necesario utilizar los tags "&#60?php //codigo php ?&#62" </p>
+        </li>
+        <li>
+            <p>Aparte del editor recomendado en el curso, ¿qué tipo de editor puedo utilizar para escribir código PHP?</p>
+            <p>Se puede utilizar cualquier editor de texto plano. Ej: notepad, notepad++, gedit, kwrite, Sublime Text.</p>
+        </li>
+        <li>
+            <p>¿Qué sentencias se utilizan para generar salida por pantalla?</p>
+            <p>Se pueden utilizar la setencia echo y la setencia print.</p>
+        </li>
+        <li>
+            <p>En el siguiente código:</p>
+            <p>$a = '2';</p>
+            <p>$a = '1' + $a;</p>
+            <p>¿Qué tipo de dato tendría la variable $a y cuál sería su valor?</p>
+            <p><strong>Respuesta:</strong></p>
 
-    2 - Aparte del editor recomendado en el curso, ¿qué tipo de editor puedo utilizar para escribir código PHP?
+            <p><fieldset>
+                <legend>Código PHP</legend>
+                <p>
+                    &#60?php<br>
+                    $a = '2';<br>
+                    echo "Inicio:&#60br&#62 Valor de la variable \$a: $a -> Tipo de dato: " . gettype($a);<br>
 
-    3 - ¿Qué sentencias se utilizan para generar salida por pantalla?
+                    $a = '1' + $a;<br>
+                    echo "&#60br&#62Final:&#60br&#62Valor de la variable \$a: $a -> Tipo de dato: " . gettype($a);<br>
+                    ?&#62
 
-    4 - En el siguiente código.
-    $a = '2';
-    $a = '1' + $a;
-    ¿Qué tipo de dato tendría la variable $a y cuál sería su valor?	
+                </p>
+            </fieldset>
+            <fieldset>
+                <legend>Salida:</legend>
+                <p>
+                    Inicio:<br>
+                    Valor de la variable $a: 2 -> Tipo de dato: string<br>
+                    Final:<br>
+                    Valor de la variable $a: 3 -> Tipo de dato: integer<br>
+                </p>
+            </fieldset></p>
+        </li>
+        <li>
+            <p>¿Qué significa que php está embebido en HTML?</p>
+            <p>Significa que se puede escribir código php dentro del código html para agregar dinamismo a la página.</p>
+        </li>
+        <li>
+            <p>¿Cuáles son las variables predefinidas de PHP para realizar el pasaje de parámetros de entrada?</p>
+            <p>
+            <ul>
+                <li>$_POST</li>
+                <li>$_GET</li>
+                <li>$_REQUEST</li>
+            </ul>
+            </p>
+        </li>
+        <li>Explique brevemente de qué manera se puede recuperar el valor de las variables de entradas.</li>
+        <li>Analizando las siguientes sentencias<br>
+            $x = 0;<br>
+            $x += 5;<br>
 
-    5 – ¿Qué significa que php está embebido en HTML?
+            y<br>
 
-    6 – ¿Cuáles son las variables predefinidas de PHP para realizar el pasaje de parámetros de entrada?
+            $x = 0;<br>
+            $x = $x + 5;<br>
 
-    7 - Explique brevemente de qué manera se puede recuperar el valor de las variables de entradas.
+            Podemos decir que<br>
+            <ul>
 
-    8 - Analizando las siguientes sentencias 	
-    $x = 0;
-    $x += 5;
+                <li>La primera posee un error de sintaxis</li>
+                <li>Son Equivalentes</li>	 
+                <li>La segunda es la forma correcta de hacerlo</li>
+                <li>No producen el mismo resultado</li>
 
-    y
+            </ul>
 
-    $x = 0;
-    $x = $x + 5;
-
-    Podemos decir que
-
-    - La primera posee un error de sintaxis
-    - Son Equivalentes	 
-    - La segunda es la forma correcta de hacerlo	 
-    - No producen el mismo resultado
-
-    (Una Opción es la Correcta)
-
-
-    9 - Realizar una variante de Hello World donde se ingrese un parámetro de entrada via GET que se llame nombre, y que en vez de decir simplemente Hello World diga Hello y el nombre ingresado. Adjuntar el código del script en la respuesta.
-
-    10 – realizar una variante del script mostrado en clase en el cual el método a utilizar no sea GET ni POST sino REQUEST. Adjuntar el código del script en la respuesta.
+            (Una Opción es la Correcta)</li>
+        <li>Realizar una variante de Hello World donde se ingrese un parámetro de entrada via GET que se llame nombre, y que en vez de decir simplemente Hello World diga Hello y el nombre ingresado. Adjuntar el código del script en la respuesta.</li>
+        <li>realizar una variante del script mostrado en clase en el cual el método a utilizar no sea GET ni POST sino REQUEST. Adjuntar el código del script en la respuesta.</li>
+    </ol>
 </body>
 </html>
