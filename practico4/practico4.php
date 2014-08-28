643858258f456104b8f9cbdc9c38b6a16c9191eb
Arcangel617 <artigue_arcangel@hotmail.com>
2014-08-28 15:30:31 -0300

    Cree un html para las respuestas del práctico

---
+++ practico4/practico4.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    1 - ¿Qué tags me conviene usar para encerrar código php?
+
+    2 - Aparte del editor recomendado en el curso, ¿qué tipo de editor puedo utilizar para escribir código PHP?
+
+    3 - ¿Qué sentencias se utilizan para generar salida por pantalla?
+
+    4 - En el siguiente código.
+    $a = '2';
+    $a = '1' + $a;
+    ¿Qué tipo de dato tendría la variable $a y cuál sería su valor?	
+
+    5 – ¿Qué significa que php está embebido en HTML?
+
+    6 – ¿Cuáles son las variables predefinidas de PHP para realizar el pasaje de parámetros de entrada?
+
+    7 - Explique brevemente de qué manera se puede recuperar el valor de las variables de entradas.
+
+    8 - Analizando las siguientes sentencias 	
+    $x = 0;
+    $x += 5;
+
+    y
+
+    $x = 0;
+    $x = $x + 5;
+
+    Podemos decir que
+
+    - La primera posee un error de sintaxis
+    - Son Equivalentes	 
+    - La segunda es la forma correcta de hacerlo	 
+    - No producen el mismo resultado
+
+    (Una Opción es la Correcta)
+
+
+    9 - Realizar una variante de Hello World donde se ingrese un parámetro de entrada via GET que se llame nombre, y que en vez de decir simplemente Hello World diga Hello y el nombre ingresado. Adjuntar el código del script en la respuesta.
+
+    10 – realizar una variante del script mostrado en clase en el cual el método a utilizar no sea GET ni POST sino REQUEST. Adjuntar el código del script en la respuesta.
+</body>
+</html>
