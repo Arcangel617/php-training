63d574341fe0d017918688477258d1389b40eb5e
Arcangel617 <artigue_arcangel@hotmail.com>
2014-09-06 03:30:06 -0300

    Se modoficó la estructura

---
+++ practicos/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Prácticos</title>
+        <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    </head>
+
+    <body>
+
+        <!-- Begin Container -->
+        <div id="container">
+
+
+
+        
---
+++ practicos/index.php
@@ -0,0 +1,10 @@
+<?php 
+
+include "header.php";
+
+echo "tenés que navegar por los directorios =) -> ";
+echo "<a href='./views'>Go!</a>";
+
+include "footer.php";
+
+?>
